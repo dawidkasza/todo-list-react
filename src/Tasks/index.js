c71070aa55e5b313d878f623fc4aa632e6bf0df1
Dawid Kasza <dawidkasza@op.pl>
2024-03-26 18:20:18 +0100

    ToDo List refactoing on react

---
+++ src/Tasks/index.js
@@ -0,0 +1,18 @@
+import "./style.css"
+
+const Tasks = (props) => (
+    <ul className="task">
+        {props.tasks.map(task => (
+            <li className={`task__item ${task.done && props.hideDoneTasks ? "task__item--hidden" : ""}`}>
+                <button className=" task__button task__button--toggleDone"> 
+                {task.done ? "✔" : ""}</button>
+            <span className={`task__content ${task.done ? "task__item--done" : ""}`}> 
+                {task.content}
+            </span>
+            <button className=" task__button task__button--remove ">🗑</button>
+            </li>
+        ))}
+    </ul>
+);
+
+export default Tasks;
