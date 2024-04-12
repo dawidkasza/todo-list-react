95bb96c43336dba38f3493151ae299dca430475c
Dawid Kasza <dawidkasza@op.pl>
2024-04-12 13:35:32 +0200

    Add navigation

---
+++ src/Navigation/styled.js
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
+
+const activeClassName = "link-active";
+
+export const List = styled.ul`
+  background: ${({ theme }) => theme.color.teal};
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  list-style: none;
+`;
+
+export const Item = styled.li`
+  margin: 20px;
+`;
+
+export const StyledNavLink = styled(NavLink).attrs(() => ({
+  activeClassName,
+}))`
+  color: ${({ theme }) => theme.color.white};
+  text-decoration: none;
+
+  &.${activeClassName} {
+    font-weight: bold;
+  }
+
+  &:hover {
+    border-bottom: 1px solid;
+  }
+`;
