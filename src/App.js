dc30b785241f38d9a5a113b873051e8f223551f3
Dawid Kasza <dawidkasza@op.pl>
2024-04-12 13:37:20 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -2,33 +2,26 @@ import { HashRouter, NavLink, Switch, Route, Redirect } from "react-router-dom";
 import TasksPage from "./Features/tasks/TasksPage";
 import TaskPage from "./Features/tasks/TaskPage";
 import AuthorPage from "./Features/author/AuthorPage";
+import { toAuthor, toTask, toTasks } from "./routes";
+import Navigation from "./Navigation";
 
 const App = () => (
   <HashRouter>
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/zadania">Zadania</NavLink>
-        </li>
-        <li>
-          <NavLink to="/autor">Autor</NavLink>
-        </li>
-      </ul>
+    <Navigation />
       <Switch>
-        <Route path="/zadania/:id">
+        <Route path={toTask()}>
           <TaskPage />
         </Route>
-        <Route path="/zadania">
+        <Route path={toTasks()}>
           <TasksPage />
         </Route>
-        <Route path="/autor">
+        <Route path={toAuthor}>
           <AuthorPage />
         </Route>
-        <Route path="/">
-          <Redirect to="/zadania" />
+        <Route>
+          <Redirect to={toTasks()}/>
         </Route>
       </Switch>
-    </nav>
   </HashRouter>
 );
 export default App;
--- src/Features/tasks/TasksPage/index.js
+++ src/Features/tasks/TasksPage/index.js
@@ -3,13 +3,14 @@ import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
+import Search from "./Search";
 
 function TasksPage() {
   return (
     <>
       <Header title="Lista Zadań" />
       <Section title="Dodaj nowe zadanie" body={<Form />} />
-
+      <Section title="Wyszukaj" body={<Search/>} />
       <Section
         title="Lista zadań"
         body={<TaskList />}
